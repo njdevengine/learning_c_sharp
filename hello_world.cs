db920eb6909eaa736c220d64d6b2da7dc0e53cba
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-06-01 23:49:07 -0400

    Update hello_world.cs

--- hello_world.cs
+++ hello_world.cs
@@ -4,9 +4,10 @@ namespace hello_world
 {
     class MainClass
     {
-        public static void Main (string[] args)
+        public static void Main(string[] args) // this is a method called main. it is called when the program begins.
         {
-            Console.WriteLine ("Hello World!");
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
