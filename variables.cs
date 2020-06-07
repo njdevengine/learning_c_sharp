dab5fd2ad150d81947abbb5f9fe4a89edc34ed99
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-06-07 14:54:23 -0400

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace hello_world
+{
+    class MainClass
+    {
+        public static void Main(string[] args) // this is a method called main. it is called when the program begins.
+        {
+            var num01 = 10;
+            Console.WriteLine(num01);
+        }
+    }
+}
