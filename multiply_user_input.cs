0093646a78797bffe97d1d366308db232965a872
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-06-07 15:37:29 -0400

    Create multiply_user_input.cs

---
+++ multiply_user_input.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace hello_world
+{
+    class MainClass
+    {
+        public static void Main(string[] args) // this is a method called main. it is called when the program begins.
+        {
+            int num01;
+            int num02;
+
+            Console.Write("We will take 2 numbers and multiply. First number: ");
+            num01 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Your Second number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number is: " + num01 * num02);
+            Console.ReadKey();
+        }
+    }
+}
