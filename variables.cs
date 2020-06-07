ce47badf41d09150295a6d3a186c1f294588e5b2
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-06-07 15:00:51 -0400

    Update variables.cs

--- variables.cs
+++ variables.cs
@@ -7,7 +7,16 @@ namespace hello_world
         public static void Main(string[] args) // this is a method called main. it is called when the program begins.
         {
             var num01 = 10;
+            double deci = 3.14;
+            bool rainy = true;
+            string phrase = "hello grandma!";
+            int number = 3;
             Console.WriteLine(num01);
+            Console.WriteLine(deci);
+            Console.WriteLine(rainy);
+            Console.WriteLine(phrase);
+            Console.WriteLine(number);
         }
     }
+}
 }
